09d9f624e541036757d4c0c458f611858f85a50e
mocxd <mark.adkins.hastings@gmail.com>
2016-02-05 13:36:48 -0500

    adds error message to gs update version script

--- updateversion.sh
+++ updateversion.sh
@@ -6,5 +6,6 @@ if [ $? -eq 0 ] ; then
     git commit -m "(gulp) auto commit"
     git push
 else
+    echo '(WARN) Not auto committing anything. This is probably because the version number has not changed and can be totally ignored.'
     exit 1
 fi
