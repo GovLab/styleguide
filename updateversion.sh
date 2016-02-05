2e4a10d2984a8c05c358af7f024571f2d1643cc3
mocxd <mark.adkins.hastings@gmail.com>
2016-02-05 13:43:07 -0500

    dont think we need to push in both places

--- updateversion.sh
+++ updateversion.sh
@@ -5,7 +5,6 @@ git add guide/index.html sass/styleguide.md
 git diff --staged | grep -q 'govlab-styleguide'
 if [ $? -eq 0 ] ; then
     git commit -m "(gulp) auto commit"
-    git push
 else
     echo '(WARN) Not auto committing anything. This is probably because the version number has not changed and can be totally ignored.'
     exit 1
