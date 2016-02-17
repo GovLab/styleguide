3ab13df87a2b6c9f460a382583e6d2063c5a1ff7
mocxd <mark.adkins.hastings@gmail.com>
2016-02-16 20:09:30 -0500

    d3 map v3

--- js/map.js
+++ js/map.js
@@ -1,55 +1,65 @@
 
 var width = 960,
-    height = 500,
-    active = d3.select(null);
+height = 500,
+active = d3.select(null);
 
 // var projection = d3.geo.albersUsa()
 var projection = d3.geo.mercator()
 // var projection = d3.geo.kavrayskiy7()
 // var projection = d3.geo.equirectangular()
-    .scale(140)
-    .translate([width / 2, height / 1.5]);
+.scale(150)
+.translate([width / 2, height / 1.5]);
 
 var path = d3.geo.path()
-    .projection(projection);
+.projection(projection);
 
-var svg = d3.select(".map").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = d3.select('.map').append('svg')
+.attr('width', width)
+.attr('height', height);
 
-svg.append("rect")
-    .attr("class", "background")
-    .attr("width", width)
-    .attr("height", height)
-    .on("click", reset);
+svg.append('rect')
+.attr('class', 'background')
+.attr('width', width)
+.attr('height', height)
+.on('click', reset);
 
-var g = svg.append("g")
-    .style("stroke-width", "1.5px");
+var g = svg.append('g')
+.style('stroke-width', '1.5px');
 
-// d3.json("/js/us.json", function(error, us) {
+
+
+// d3.json('/js/us.json', function(error, us) {
 //   if (error) throw error;
 
-//   g.selectAll("path")
+//   g.selectAll('path')
 //       .data(topojson.feature(us, us.objects.states).features)
-//     .enter().append("path")
-//       .attr("d", path)
-//       .attr("class", "feature")
-//       .on("click", clicked);
+//     .enter().append('path')
+//       .attr('d', path)
+//       .attr('class', 'feature')
+//       .on('click', clicked);
 
-//   g.append("path")
+//   g.append('path')
 //       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-//       .attr("class", "mesh")
-//       .attr("d", path);
+//       .attr('class', 'mesh')
+//       .attr('d', path);
 // });
 
-queue()
-    .defer(d3.json, "/js/world.json")
-    .defer(d3.tsv, "/js/world-country-names.tsv")
-    .await(ready);
+function shade(rgb, v) {
+  for (var i in rgb) { rgb[i] = rgb[i]*v > 255 ? 255 : rgb[i]*v; }
+    return rgb;
+}
 
-function ready(error, world, names) {
+queue()
+.defer(d3.json, '/js/world.json')
+.defer(d3.json, '/js/studies.json')
+.defer(d3.tsv, '/js/world-country-names.tsv')
+.await(ready);
+
+function ready(error, world, studies, names) {
   if (error) throw error;
 
+
+  // get country names
   var countries = topojson.feature(world, world.objects.countries).features;
   countries = countries.filter(function(d) {
     return names.some(function(n) {
@@ -59,75 +69,163 @@ function ready(error, world, names) {
     return a.name.localeCompare(b.name);
   });
 
-  g.selectAll("path")
-      .data(countries)
-      .enter().append("path")
-      .attr("d", path)
-      .attr("class", "feature")
-      .attr("id", function(d, i) { return countries[i].name; })
-      .on("click", clicked);
+  // draw map
+  g.selectAll('path')
+  .data(countries)
+  .enter().append('path')
+  .attr('d', path)
+  .attr('class', 'feature')
+  .attr('id', function(d, i) { return countries[i].name.replace(' ', '-'); })
+  .on('click', clicked);
 
-  g.append("path")
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-      .attr("class", "mesh")
-      .attr("d", path);
+  g.append('path')
+  .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+  .attr('class', 'mesh')
+  .attr('d', path);
 
+
+
+  // draw bubbles
+  var s = studies.children;
+  var counts = {};
+
+  for (var i in s) {
+    var loc = s[i].location.replace(' ', '-');
+    if (!(s[i].impact in counts)) {
+      counts[s[i].impact] = {};
+    }
+    if (loc in counts[s[i].impact]) {
+      counts[s[i].impact][loc]['count']++;
+      if (counts[s[i].impact][loc]['count'] > 1) {
+        studies.children[i].duplicate = true;
+      }
+    } else {
+      counts[s[i].impact][loc] = {'count' : 1, 'dup' : 0};
+    }
+  }
+
+  // console.log(s);
+
+  var diameter = 120; // diameter of container circles to pack bubbles into
+  var base = 4; // log base for bubbles size curve
+  var scale = 80; // multiplier for bubbles size curve
+
+  for (var i in studies.children) {
+    var l = s[i].location.replace(' ', '-');
+    studies.children[i].size = (Math.log(counts[s[i].impact][l]['count'] + 1)/Math.log(base))*scale;
+  }
+
+  var pack = d3.layout.pack()
+  .size([diameter, diameter])
+  .value(function(d) {
+    return d.size;
+  });
+
+  var dup = 0;
+
+  var node = svg.selectAll('svg')
+  .data(pack(studies).filter(function(d) { return !d.children && !d.duplicate; }))
+  .enter().append('g')
+  .attr('class', function(d, i) {
+    // var l = d.location.replace(' ', '-');
+    // if (counts[d.impact][l]['dup']) { counts[d.impact][l]['dup']--; return 'remove'; }
+    return 'node';
+  })
+  .attr('transform', function(d, i) {
+    var country = g.select('#' + d.location.replace(' ', '-')).datum();
+    var b = path.bounds(country),
+    x = (b[0][0] + b[1][0]) / 2,
+    y = (b[0][1] + b[1][1]) / 2;
+    if (d.location === 'United States') {
+      // Translate the coords manually for USA
+      // This is a workaround to deal with the fact that the bounding box of
+      // USA expands to the entire width of the map due to Alaska's islands
+      x = x/2.15;
+      y = y*1.25;
+    }
+
+    x = (d.x-diameter/2)+x;
+    y = (d.y-diameter/2)+y;
+
+    return 'translate(' + x + ',' + y + ')';
+  });
+
+  node.append('title')
+  .text(function(d) { return d.title; });
+
+  node.append('circle')
+  .attr('r', function(d) {
+    return d.size/2;
+  })
+  .style('fill', function(d) {
+    var countsarr = [];
+    for (var i in counts[d.impact]) { countsarr.push(counts[d.impact][i]['count']); }
+      var max = Math.max.apply(null, countsarr);
+    var lighten = 1.8;
+    var c = counts[d.impact][d.location.replace(' ', '-')]['count']*lighten;
+    var s = (c > max ? max : c)/max;
+    // var s = 1;
+    var blue = [0, 138, 179];
+    var orange = [238, 91, 67];
+    var yellow = [194, 195, 59];
+    var fuchsia = [173, 0, 84];
+    if (d.impact === 'government') {
+      return d3.rgb.apply(null, shade(blue, s));
+    } else if (d.impact === 'citizens') {
+      return d3.rgb.apply(null, shade(orange, s));
+    } else if (d.impact === 'opportunity') {
+      return d3.rgb.apply(null, shade(yellow, s));
+    } else if (d.impact === 'problems') {
+      return d3.rgb.apply(null, shade(fuchsia, s));
+    }
+    return d3.rgb(128, 128, 128);
+  });
+
+  node.append('text')
+  .attr('dy', '.3em')
+  .style('text-anchor', 'middle')
+  .text(function(d) { return counts[d.impact][d.location.replace(' ', '-')]['count']; });
 
 
   // console.log(countries);
 
-  // g.selectAll("path")
-  //     .attr("id", function(d, i) {
+  // g.selectAll('path')
+  //     .attr('id', function(d, i) {
   //       return countries[i % countries.length].name;
   //     })
 
-  g.select("#Antarctica").remove();
+  // note: for some reason can't remove top level g element?
+  // node.selectAll('.duplicates:not(:first-child) circle').remove(); // this is because there is no good way to conditionally .append
+  // node.selectAll('.remove circle, .remove text, .remove title').remove();
+
+  g.select('#Antarctica').remove();
 }
-
-// var color = d3.scale.category10();
-
-// d3.json("/js/world.json", function(error, world) {
-//   if (error) throw error;
-//   var countries = topojson.feature(world, world.objects.countries).features,
-//       neighbors = topojson.neighbors(world.objects.countries.geometries);
-//   svg.selectAll(".country")
-//       .data(countries)
-//     .enter().insert("path", ".graticule")
-//       .attr("class", "country")
-//       .attr("d", path)
-//       .style("fill", function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); });
-//   svg.insert("path", ".graticule")
-//       .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-//       .attr("class", "boundary")
-//       .attr("d", path);
-// });
-
 
 function clicked(d) {
   if (active.node() === this) return reset();
-  active.classed("active", false);
-  active = d3.select(this).classed("active", true);
+  active.classed('active', false);
+  active = d3.select(this).classed('active', true);
 
   var bounds = path.bounds(d),
-      dx = bounds[1][0] - bounds[0][0],
-      dy = bounds[1][1] - bounds[0][1],
-      x = (bounds[0][0] + bounds[1][0]) / 2,
-      y = (bounds[0][1] + bounds[1][1]) / 2,
-      scale = .9 / Math.max(dx / width, dy / height),
-      translate = [width / 2 - scale * x, height / 2 - scale * y];
+  dx = bounds[1][0] - bounds[0][0],
+  dy = bounds[1][1] - bounds[0][1],
+  x = (bounds[0][0] + bounds[1][0]) / 2,
+  y = (bounds[0][1] + bounds[1][1]) / 2,
+  scale = .9 / Math.max(dx / width, dy / height),
+  translate = [width / 2 - scale * x, height / 2 - scale * y];
 
-  g.transition()
+  // g.transition()
+      // .duration(750)
+      // .style('stroke-width', 1.5 / scale + 'px');
+      // .attr('transform', 'translate(' + translate + ')scale(' + scale + ')');
+    }
+
+    function reset() {
+      active.classed('active', false);
+      active = d3.select(null);
+
+      g.transition()
       .duration(750)
-      .style("stroke-width", 1.5 / scale + "px")
-      .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-}
-
-function reset() {
-  active.classed("active", false);
-  active = d3.select(null);
-
-  g.transition()
-      .duration(750)
-      .style("stroke-width", "1.5px")
-      .attr("transform", "");
-}
+      .style('stroke-width', '1.5px')
+      .attr('transform', '');
+    }
